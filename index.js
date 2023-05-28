9ccc0fe94e6a22cac4943f08cee56798a8b07661
Juebele <eronuebele@gmail.com>
2023-05-28 18:46:38 -0400

    added constructor function

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-const fs = require("fs");
+const fs = require(fs);
 const inquirer = require("inquirer");
-const (Circle, Triangle, Square) = require("./lib/shapes");
+const {Circle, Triangle, Square} = require("./lib/shapes");
+
+class svg{
+    constructor(){
+        this.textElement = ''
+        this.shapeElement = ''
+    }
+    render(){
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
+    }
+    setText (text,color){
+        this.textElement = `text x="150" y="150" font-size="75" fill="${color}">${text}</text>`
+    }
+    setShape (shape){
+        this.shapeElement = shape.render()
+    }
+}
 
 function run () {
     inquirer.prompt(questions).then((responses) => {
